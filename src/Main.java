c5b28a9eb8728df570220ae263d03972728f65e3
david <diglesiasperez@danielcastealo.com>
2025-04-09 23:40:33 +0200

    Refactorizado método getScore en métodos separados para mejorar legibilidad

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,51 @@
 public class Main {
     /**
-     * metodo para devolver la puntuacion de tenis
+     * Metodo para devolver la puntuación de tenis
      */
     public static String getScore(int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-
         if (m_score1 == m_score2) {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+            return getEqualScore(m_score1);
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getAdvantageOrWin(m_score1, m_score2);
+        } else {
+            return getStandardScore(m_score1, m_score2);
+        }
+    }
 
+    private static String getEqualScore(int score) {
+        return switch (score) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            case 3 -> "Forty-All";
+            default -> "Deuce";
+        };
+    }
+
+    private static String getAdvantageOrWin(int m_score1, int m_score2) {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private static String getStandardScore(int m_score1, int m_score2) {
+        StringBuilder score = new StringBuilder();
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = m_score1;
+            else {
+                score.append("-");
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
+                case 0 -> score.append("Love");
+                case 1 -> score.append("Fifteen");
+                case 2 -> score.append("Thirty");
+                case 3 -> score.append("Forty");
             }
         }
-        else if (m_score1 >=4 || m_score2 >=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-
-        }
-    return score;
+        return score.toString();
     }
 }
